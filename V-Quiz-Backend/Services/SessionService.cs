058142be9347d18e530cdf520d0b5203c1bdee05
Thomas Hallström <minimoto@hotmail.com>
2025-12-02 11:32:21 +0100

    Cleaning up

--- V-Quiz-Backend/Services/SessionService.cs
+++ V-Quiz-Backend/Services/SessionService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using V_Quiz_Backend.Models;
+﻿using V_Quiz_Backend.Models;
 using V_Quiz_Backend.Repository;
 
 namespace V_Quiz_Backend.Services
