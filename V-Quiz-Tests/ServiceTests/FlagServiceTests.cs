a5d1753376c3167559137fff973d4676a97f6417
MrMinken <minimoto@hotmail.com>
2025-12-09 09:20:21 +0100

    adding flagtest

---
+++ V-Quiz-Tests/ServiceTests/FlagServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using V_Quiz_Backend.Services;
+
+namespace V_Quiz_Tests.ServiceTests
+{
+    public class FlagServiceTests
+    {
+        private readonly FlagService _flagService;
+    }
+}
